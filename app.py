5761bdb30dbb6e1688ffcba73bd10019aa239521
Bharat <74244751+Bharat-3@users.noreply.github.com>
2025-06-20 06:28:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask on OpenShift!"
+    return " Hello from Flask on OpenShift with SSL!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)  #  Must be 8080
